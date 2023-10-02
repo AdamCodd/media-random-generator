18fe5507bdae148026bb14b1b52151fa5b7701f2
AdamCodd <49641859+AdamCodd@users.noreply.github.com>
2023-10-02 12:20:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -197,3 +197,20 @@ function saveBlockedUrls() {
     };
   }
 }
+
+// Return to top
+document.addEventListener('DOMContentLoaded', function() {
+    const backToTopButton = document.getElementById('back-to-top');
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) {
+            backToTopButton.style.display = 'block';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    });
+
+    backToTopButton.addEventListener('click', function() {
+        window.scrollTo(0, 0);
+    });
+});
